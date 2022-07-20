a9e038b865a94a7b4fc9503fea88047ef458a9fb
Samuel Henz <samuel@henz.fr>
2022-07-20 12:54:01 +0200

    Update chosenMaxLife

--- control-03-monster-killer-starting-project/assets/scripts/app.js
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -3,10 +3,21 @@ const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
 const HEAL_VALUE = 20;
 
-let chosenMaxLife = 100;
+const MODE_ATTACK = "ATTACK";
+const MODE_STRONG_ATTACK = "STRONG_ATTACK";
+
+const enteredValue = prompt("Choose MaxLife: ", "100");
+let chosenMaxLife = parseInt(enteredValue);
+console.log(chosenMaxLife);
+if (isNaN(chosenMaxLife) || chosenMaxLife <= 0) {
+    chosenMaxLife = 100;
+}
+
 let currentPlayerHealth = chosenMaxLife;
 let currentMonsterHealth = chosenMaxLife;
 let hasBonusLife = true;
+
+adjustHealthBars(chosenMaxLife);
 
 function reset() {
     currentPlayerHealth = chosenMaxLife;
@@ -43,9 +54,9 @@ function endTurn() {
 
 function attackMonster(mode) {
     let maxDmg;
-    if (mode === "ATTACK") {
+    if (mode === MODE_ATTACK) {
         maxDmg = ATTACK_VALUE;
-    } else if (mode === "STRONG_ATTACK"){
+    } else if (mode === MODE_STRONG_ATTACK){
         maxDmg = STRONG_ATTACK_VALUE;
     }
     const damage = dealMonsterDamage(maxDmg);
@@ -53,11 +64,11 @@ function attackMonster(mode) {
     endTurn();
 }
 function onAttack() {
-    attackMonster("ATTACK");
+    attackMonster(MODE_ATTACK);
 }
 
 function onStrongAttack() {
-    attackMonster("STRONG_ATTACK");
+    attackMonster(MODE_STRONG_ATTACK);
 }
 
 function onHeal() {
