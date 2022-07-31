e91eaa271bca6f8722a73fbe63566bc6e0f5fae7
Samuel Henz <samuel@henz.fr>
2022-07-31 14:16:34 +0200

    assignement dOM1

---
+++ dom-assignment-start/assignment.js
@@ -0,0 +1,8 @@
+document.getElementById("task-1").style.color = "white";
+document.querySelector("#task-1").style.backgroundColor = "black"
+
+document.querySelector("title").textContent = "Assignment - Solved!";
+document.title = "Assignment - Solved!";
+
+const h1 = document.getElementsByTagName("h1")[0];
+h1.textContent = "Assignment - Solved!";
