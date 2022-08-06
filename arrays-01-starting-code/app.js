0c390d954cfa5c3858639f3342f3d5c00149fe32
Samuel Henz <samuel@henz.fr>
2022-08-06 12:43:34 +0200

    find, findIndex, indexOf, lastIndexOf , incluides

--- arrays-01-starting-code/app.js
+++ arrays-01-starting-code/app.js
@@ -30,13 +30,56 @@ console.log(yetMoreNumbers);
 // }
 
 const hobbies = ["Sports", "Cooking"];
-hobbies.push("Coding");
-hobbies.unshift("Reading");
-console.log(hobbies);
-const poppedValue = hobbies.pop();
-hobbies.shift();
-console.log(hobbies);
 
-hobbies[1] = "Coding";
-// hobbies[5] = "Reading"; // Extend the array and add undefined to between slots
-console.log(hobbies);
+/* Push, pop, shift and unshift */
+
+// hobbies.push("Coding");
+// hobbies.unshift("Reading");
+// console.log(hobbies);
+// const poppedValue = hobbies.pop();
+// hobbies.shift();
+// console.log(hobbies);
+
+// hobbies[1] = "Coding";
+// // hobbies[5] = "Reading"; // Extend the array and add undefined to between slots
+// console.log(hobbies);
+
+/* Splice Method */
+
+// hobbies.splice(1, 0, "Good Food"); // Insert good food at the index 1 and remove zero elements
+// console.log(hobbies);
+
+// const removedElems = hobbies.splice(-1, 1); // Remove the last element from the array
+// console.log(hobbies, removedElems);
+
+// hobbies.splice(0); // Remove all elements from index zero
+// console.log(hobbies);
+
+// const testResults = [3, 4.9, -10, 9];
+// const storedResults = testResults.slice(2); // Return testResults from index 2 
+
+const testResults = [3, 4.9, -10, 9];
+const storedResults = testResults.concat([22, 7.9, 89]);
+const personalData = [{name: "Jack"}, {name: "Stephen"}];
+
+testResults.push(3);
+
+console.log(testResults, storedResults);
+console.log(testResults.indexOf(3)); // Return the first entry find in the array 
+console.log(testResults.lastIndexOf(3)); // Return the first entry starting from the end
+console.log(personalData.indexOf({name: "Jack"})) // Return -1 because of reference of objects
+
+const jack = personalData.find((person, idx, persons) => {
+    return person.name === "Jack"; // Find return a reference not a copy !
+});
+
+const stephenIndex = personalData.findIndex((person, idx, persons) => {
+    return person.name === "Stephen";
+}); 
+
+console.log(jack);
+// jack.name = "Jack II";
+console.log(stephenIndex);
+console.log(personalData);
+
+console.log(testResults.includes(4.9)); // .includes in JS not .contain
